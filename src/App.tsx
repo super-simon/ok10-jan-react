ae3eabbbfe0e1d9678e0e996445f4e72f9443ae5
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-05-10 21:00:32 +0300

    useCallback, useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { memo, useState } from "react";
+import { FC, memo, useCallback, useMemo, useState } from "react";
 
-const A = memo(() => {
+const A: FC<any> = memo(({ foobar }) => {
   console.log("A render");
+  foobar();
   return <>Hello!</>;
 });
 
 const App = () => {
   console.log("App render");
   const [counter, setCouner] = useState(0);
+  const foobar = useCallback(() => {}, []);
+
+  const arr = useMemo(() => {
+    return [];
+  }, []);
+
   return (
     <div>
-      <A />
+      <A foobar={foobar} arr={arr} />
       <button onClick={() => setCouner(counter + 1)}> click me</button>
     </div>
   );
