556c95350b238c1de2090f0443377ecbc9e6cad1
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-06-14 16:53:25 +0300

    react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,78 @@
+import { useState } from "react";
+import {
+  decrement,
+  decrementBy,
+  increment,
+  incrementBy,
+  reset,
+} from "./redux/slices/slice1";
+import { useAppDispatch, useAppSelector } from "./redux/store";
+
 const App = () => {
-  return <div>App</div>;
+  const [by, setBy] = useState(5);
+  const { value } = useAppSelector((state) => state.slice1);
+  const dispatch = useAppDispatch();
+  return (
+    <div>
+      <h2>{value}</h2>
+      <button
+        onClick={() => {
+          dispatch(incrementBy(10));
+        }}
+      >
+        Increment by 10
+      </button>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrementBy(10));
+        }}
+      >
+        Decrement by 10
+      </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        Reset
+      </button>
+      <br />
+      <input
+        type="number"
+        onChange={(e) => setBy(+e.target.value)}
+        value={by}
+      />
+      <br />
+      <button
+        onClick={() => {
+          dispatch(incrementBy(by));
+        }}
+      >
+        Increment By
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrementBy(by));
+        }}
+      >
+        Decrement By
+      </button>
+    </div>
+  );
 };
 
 export default App;
